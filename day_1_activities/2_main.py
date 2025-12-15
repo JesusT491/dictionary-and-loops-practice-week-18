6d1e39b5054804c46f61aab136555e729e4bfeea
JesusT491 <jtorres217@cps.edu>
2025-12-15 20:48:15 +0000

    commit2

--- day_1_activities/2_main.py
+++ day_1_activities/2_main.py
@@ -43,10 +43,33 @@ import student_data
 
 # print(student_data.students)
 students = student_data.students
-print(len(students))
-print(students[0]['Combo,Name'])
-print(students[0]['Email'][0])
-print(students[0]['Email'][1])
+# print(len(students))
+
+# #student 1
+# print(students[0]['Combo,Name'])
+# print(students[0]['Email'][0])
+
+# print(students[0]['FName'])
+# print(students[0]['LName'])
+# print(students[0]['GL'])
+# print()
+
+# #student 2
+# print(students[1]['FName'])
+# print(students[1]['Email'][1])
+# print(students[0]['GL'])
+# print(students[0]['HR'])
+# print()
+
+# #student last
+# print(students[-1]['FName'])
+# print(students[-1]['Email'][1])
+# print(students[-1]['CPSID'])
+# print(students[-1]['GL'])
+# print(students[-1]['HR'])
+
+
+
 
 # What is being counted?
 
@@ -68,11 +91,31 @@ print(students[0]['Email'][1])
 #and printing the name and email of the students
 #we are also printing a line of underscores to separate the students
 #we are also printing a line of underscores to separate the students
-for student in students:
-    print(student['Combo,Name'])
-    print(student['Email'][0])
-    print(student['Email'][1])
-    print("_"*25)
+
+
+#MY CODE
+# for student in students:
+#     print(student['Combo,Name'])
+#     print(student['Email'][0])
+#     print(student['Email'][1])
+#     print(student['HR'])
+#     print(student['GL'])
+#     print(student['CPSID'])
+
+#     if student['GL'] >= 10:
+#         print("Can go off campus ")
+#     else:
+#         print("Can NOT go off campus")
+
+#     if student['LName'][0] == "R":
+#         print(['FName'])
+#         print(['HR'])
+
+#     if student['LName'][0] == "L":
+#         break
+
+#     print("_"*25)
+
 
 # What does the loop variable student represent on each pass?
 
@@ -88,6 +131,7 @@ for student in students:
 # we are asking the user to input their name
 # then we are checking if the name is in the data
 # if the name is in the data we are printing the name and "this works"
+
 name = input("what is you name?") 
 for student in students:
     if name == student['Combo,Name']:
